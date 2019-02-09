86de3a54ac1caa6073d2ff8d7615f37e8b3cfddf
clintonjones <clinton@totient.capital>
2019-02-09 13:35:06 -0800

    + material ui

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import { connect } from 'react-redux'
 
 import { fetchBalances } from './actions/balanceActions'
 
-import logo from './logo.svg'
-import './App.scss'
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header>
+          <img src={'https://cometverse.com/comet/3.svg'} className="vip-logo" alt="logo" />
+          <h1>VIP Wallet</h1>
         </header>
+        <main>
+          <span>Lots of cool stuff goes here.</span>
+        </main>
+        <footer>
+          <span>&copy; 2019 the comets of cometverse</span>
+        </footer>
       </div>
     )
   }
